0f23a5e31e0695cc085c4f6d3a080a3a9b11e70c
tom <tom@qq.com>
2019-04-12 22:00:50 +0800

    测试冲突

--- src/main/java/com/atguigu/git/GitMain.java
+++ src/main/java/com/atguigu/git/GitMain.java
@@ -1,8 +1,10 @@
-package com.atguigu.git;
+ package com.atguigu.git;
 
 public class GitMain {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hello");// AAA
 		System.out.println("hello");
 	}
 }
